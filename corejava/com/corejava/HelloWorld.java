37646552662ff31d723ac954e5f034a6da4f9938
mahesh7774 <j2se.mahesh@gmail.com>
2017-07-13 20:51:51 +0530

    4th time commit

--- corejava/com/corejava/HelloWorld.java
+++ corejava/com/corejava/HelloWorld.java
@@ -5,8 +5,8 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("YES JENKSIN CALLED: I AM DANCING ******");
-				System.out.println("NOW Second Time WIth 3 Nodes ");
+		System.out.println("TEST");
+	 	System.out.println("NOW Second Time WIth 3 Nodes ");
 
 	}
 }
