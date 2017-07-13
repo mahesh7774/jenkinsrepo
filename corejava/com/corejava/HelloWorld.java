4477e302ba59d09e417683630b53c30b84b5e6e1
Mahesh <j2se.mahesh@gmail.com>
2017-07-13 20:38:28 +0530

    This 3rd time modification on System()

--- corejava/com/corejava/HelloWorld.java
+++ corejava/com/corejava/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("YES JENKSIN CALLED: I AM DANCING ******");
+				System.out.println("NOW Second Time WIth 3 Nodes ");
+
 	}
 }
