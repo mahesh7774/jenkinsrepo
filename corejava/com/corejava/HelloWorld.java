d06088ba7ec4bd461ba5d2d869cf680a396e4170
Mahesh <j2se.mahesh@gmail.com>
2017-07-15 22:24:46 +0530

    Check 5

--- corejava/com/corejava/HelloWorld.java
+++ corejava/com/corejava/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
 		System.out.println("NOW Second Time WIth 3 Nodes ");
 		System.out.println("NOW 5Th time testd-based on Bitbcuket changes... ");
 =======
-		System.out.println("TEST");
+		System.out.println("TEST///Remove");
 	 	System.out.println("NOW Second Time WIth 3 Nodes ");
 >>>>>>> 37646552662ff31d723ac954e5f034a6da4f9938
 
